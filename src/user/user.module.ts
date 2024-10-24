4b1cf12d119e644b93829d76cb58f49b061c18fb
Muhammad Owais <rahneverd@gmail.com>
2024-10-24 16:51:12 +0500

    typeorm dependencies

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserHistory } from './entities/user-history.entity';
+import { User } from './entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, UserHistory])],
   controllers: [UserController],
   providers: [UserService],
 })
