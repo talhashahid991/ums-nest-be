084cab38fddb3c4e50cf19cb484e0db1b9d69980
Muhammad Owais <rahneverd@gmail.com>
2024-10-28 17:53:05 +0500

    lovCategory module updated

--- src/list-of-values/list-of-values.module.ts
+++ src/list-of-values/list-of-values.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ListOfValuesService } from './list-of-values.service';
 import { ListOfValuesController } from './list-of-values.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  // imports: [TypeOrmModule.forFeature([User, UserHistory])],
   controllers: [ListOfValuesController],
   providers: [ListOfValuesService],
 })
