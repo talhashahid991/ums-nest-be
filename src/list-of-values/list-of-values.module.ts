08943617d05f528fa1d3343f64f34c3b9639cfaa
Muhammad Owais <rahneverd@gmail.com>
2024-10-29 09:51:06 +0500

    updated list-of-values module

--- src/list-of-values/list-of-values.module.ts
+++ src/list-of-values/list-of-values.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ListOfValuesService } from './list-of-values.service';
 import { ListOfValuesController } from './list-of-values.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ListOfValues } from './entities/list-of-values.entity';
+import { ListOfValuesHistory } from './entities/list-of-values-history.entity';
 
 @Module({
-  // imports: [TypeOrmModule.forFeature([User, UserHistory])],
+  imports: [TypeOrmModule.forFeature([ListOfValues, ListOfValuesHistory])],
   controllers: [ListOfValuesController],
   providers: [ListOfValuesService],
 })
