8512c1125bc6ce4617e507cedf096bc0b97bcff1
Muhammad Owais <rahneverd@gmail.com>
2024-10-31 16:13:51 +0500

    added cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
