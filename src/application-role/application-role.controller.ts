9d87c7d3afc9020ff1683aaa9d37b0c93441a8e6
Muhammad Owais <rahneverd@gmail.com>
2024-11-06 13:18:07 +0500

    application-role resource added

---
+++ src/application-role/application-role.controller.ts
@@ -0,0 +1,122 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { ApplicationRoleService } from './application-role.service';
+import JwtAuthGuard from 'src/auth/jwt-auth.guard';
+import { CreateDto } from './dto/create.dto';
+import { addStandardParameters } from 'src/utils/commonFunctions';
+import { API_SUCCESS_MESSAGE, LID_CREATED_ID } from 'src/utils/constants';
+import { isEmpty } from 'lodash';
+import * as dayjs from 'dayjs';
+import { RestResponse } from 'src/utils/restResponse';
+import { FindAllDto } from './dto/find-all.dto';
+
+@Controller('application-role')
+export class ApplicationRoleController {
+  constructor(private readonly mainService: ApplicationRoleService) {}
+
+  @UseGuards(JwtAuthGuard)
+  // @UseGuards(RoleGuard(Role.FullLovCategoryAccess, Role.AddLovCategory))
+  @Post('create')
+  async create(@Request() req: any, @Body() createDto: CreateDto) {
+    try {
+      const res = await Promise.all(
+        createDto?.data?.map((createPayload) => {
+          const standardParams = addStandardParameters(req.user, createPayload);
+          return this.mainService.create({
+            ...standardParams,
+            dmlStatus: LID_CREATED_ID,
+            dmlTimestamp: dayjs().format(),
+          });
+        }),
+      );
+
+      if (!isEmpty(res)) {
+        return RestResponse.success(res, API_SUCCESS_MESSAGE);
+      } else {
+        return RestResponse.notFound(res);
+      }
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  @UseGuards(JwtAuthGuard)
+  // // @UseGuards(RoleGuard(Role.FullLovCategoryAccess, Role.FindAllLovCategory))
+  @Post('findAll')
+  async findAll(@Request() req: any, @Body() findAllDto: FindAllDto) {
+    try {
+      const res = await Promise.all(
+        findAllDto?.data?.map((findPayload) => {
+          const standardParams = addStandardParameters(req.user, findPayload);
+          return this.mainService.findAll(
+            standardParams,
+            findAllDto?.pagination,
+          );
+        }),
+      );
+      if (!isEmpty(res) && !isEmpty(res[0])) {
+        return RestResponse.success(res, API_SUCCESS_MESSAGE);
+      } else {
+        return RestResponse.notFound(res);
+      }
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  // @UseGuards(JwtAuthGuard)
+  // // @UseGuards(RoleGuard(Role.FullLovCategoryAccess, Role.UpdateLovCategory))
+  // @Post('update')
+  // async update(@Request() req: any, @Body() updateDto: UpdateDto) {
+  //   try {
+  //     const res = await Promise.all(
+  //       updateDto?.data?.map((updatePayload) => {
+  //         const standardParams = addStandardParameters(req.user, updatePayload);
+  //         return this.mainService.update({
+  //           ...standardParams,
+  //           dmlStatus: LID_UPDATE_ID,
+  //           dmlTimestamps: dayjs().format(),
+  //         });
+  //       }),
+  //     );
+  //     if (!isEmpty(res)) {
+  //       return RestResponse.success(res, API_SUCCESS_MESSAGE);
+  //     } else {
+  //       return RestResponse.notFound(res);
+  //     }
+  //   } catch (e) {
+  //     throw e;
+  //   }
+  // }
+
+  // @UseGuards(JwtAuthGuard)
+  // // @UseGuards(RoleGuard(Role.FullLovCategoryAccess, Role.DeleteLovCategory))
+  // @Post('delete')
+  // async remove(@Request() req: any, @Body() deleteDto: DeleteDto) {
+  //   try {
+  //     const res = await Promise.all(
+  //       deleteDto.data.map((findPayload) => {
+  //         const standardParams = addStandardParameters(req.user, findPayload);
+  //         return this.mainService.delete({
+  //           ...standardParams,
+  //           dmlStatus: LID_DELETE_ID,
+  //           dmlTimestamps: dayjs().format(),
+  //         });
+  //       }),
+  //     );
+  //     if (!isEmpty(res)) {
+  //       return RestResponse.success(res, API_SUCCESS_MESSAGE);
+  //     } else {
+  //       return RestResponse.notFound(res);
+  //     }
+  //   } catch (e) {
+  //     throw e;
+  //   }
+  // }
+}
---
+++ src/application-role/application-role.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ApplicationRoleService } from './application-role.service';
+import { ApplicationRoleController } from './application-role.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApplicationRole } from './entities/application-role.entity';
+import { ApplicationRoleHistory } from './entities/application-role-history.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([ApplicationRole, ApplicationRoleHistory]),
+  ],
+  controllers: [ApplicationRoleController],
+  providers: [ApplicationRoleService],
+})
+export class ApplicationModule {}
