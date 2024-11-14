c7e56db15801505d37b571e7864d53fe235516c3
Muhammad Owais <rahneverd@gmail.com>
2024-11-14 15:31:26 +0500

    organization entity updated

--- src/organization/entities/organization.entity.ts
+++ src/organization/entities/organization.entity.ts
@@ -1,1 +1,58 @@
-export class Organization {}
+import { Application } from 'src/application/entities/application.entity';
+import { ListOfValues } from 'src/list-of-values/entities/list-of-values.entity';
+import { LovCategory } from 'src/lov-category/entities/lov-category.entity';
+import { User } from 'src/user/entities/user.entity';
+import { LID_ACTIVE_ID, LID_CREATED_ID } from 'src/utils/constants';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  JoinColumn,
+  ManyToOne,
+  OneToOne,
+} from 'typeorm';
+
+@Entity({ name: 'organization' })
+export class Organization {
+  @PrimaryGeneratedColumn({ name: 'organization_id' })
+  organizationId: number;
+
+  @Column({ name: 'title' })
+  title: string;
+
+  @Column({ name: 'description', nullable: true })
+  description: string;
+
+  @OneToOne(() => User, (x) => x.userId)
+  @JoinColumn({ name: 'owner_id' })
+  @Column({
+    name: 'owner_id',
+  })
+  applicationId: number;
+
+  @ManyToOne(() => ListOfValues, (Lov) => Lov.listOfValuesId)
+  @JoinColumn({ name: 'lov_status_id' })
+  @Column({
+    name: 'lov_status_id',
+    default: LID_ACTIVE_ID,
+  })
+  lovStatusId: number;
+
+  // dml
+
+  @ManyToOne(() => ListOfValues, (lov) => lov.listOfValuesId)
+  @JoinColumn({ name: 'dml_status' })
+  @Column({
+    name: 'dml_status',
+    default: LID_CREATED_ID,
+  })
+  dmlStatus: number;
+
+  @ManyToOne(() => User, (user) => user.userId)
+  @JoinColumn({ name: 'dml_user_id' })
+  @Column({ name: 'dml_user_id' })
+  dmlUserId: number;
+
+  @Column({ name: 'dml_timestamp', type: 'timestamptz' })
+  dmlTimestamp: Date;
+}
