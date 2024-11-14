a3019dfadf39274e63f7ea8f864c5bdab68964a3
Muhammad Owais <rahneverd@gmail.com>
2024-11-14 15:59:21 +0500

    organization dtos added - #7

---
+++ src/organization/dto/update.dto.ts
@@ -0,0 +1,39 @@
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+
+export class UpdateDataPayloadDto {
+  @IsNotEmpty()
+  @IsNumber()
+  organizationId: number;
+
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  ownerId?: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  lovStatusId?: number;
+}
+
+export class UpdateDto {
+  @IsNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => UpdateDataPayloadDto)
+  @IsArray()
+  data: UpdateDataPayloadDto[];
+}
