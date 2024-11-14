8e68ba0486df9033c80235ddf08697010018ee22
Muhammad Owais <rahneverd@gmail.com>
2024-11-14 17:32:23 +0500

    updated controller and service with endpoints - organization - #7

--- src/organization/organization.controller.ts
+++ src/organization/organization.controller.ts
@@ -1,17 +1,124 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Post, Body, Request, UseGuards } from '@nestjs/common';
 import { OrganizationService } from './organization.service';
+import JwtAuthGuard from 'src/auth/jwt-auth.guard';
+import { CreateDto } from './dto/create.dto';
+import { addStandardParameters } from 'src/utils/commonFunctions';
+import {
+  API_SUCCESS_MESSAGE,
+  LID_CREATED_ID,
+  LID_DELETE_ID,
+  LID_UPDATE_ID,
+} from 'src/utils/constants';
+import { isEmpty } from 'lodash';
+import * as dayjs from 'dayjs';
+import { RestResponse } from 'src/utils/restResponse';
+import { FindAllDto } from './dto/find-all.dto';
+import { UpdateDto } from './dto/update.dto';
+import { DeleteDto } from './dto/delete.dto';
 
 @Controller('organization')
 export class OrganizationController {
-  constructor(private readonly organizationService: OrganizationService) {}
+  constructor(private readonly mainService: OrganizationService) {}
+
+  @UseGuards(JwtAuthGuard)
+  // @UseGuards(RoleGuard(Role.FullLovCategoryAccess, Role.AddLovCategory))
+  @Post('create')
+  async create(@Request() req: any, @Body() createDto: CreateDto) {
+    try {
+      const res = await Promise.all(
+        createDto?.data?.map((createPayload) => {
+          const standardParams = addStandardParameters(req.user, createPayload);
+          return this.mainService.create({
+            ...standardParams,
+            dmlStatus: LID_CREATED_ID,
+            dmlTimestamp: dayjs().format(),
+          });
+        }),
+      );
+
+      if (!isEmpty(res)) {
+        return RestResponse.success(res, API_SUCCESS_MESSAGE);
+      } else {
+        return RestResponse.notFound(res);
+      }
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  @UseGuards(JwtAuthGuard)
+  // // @UseGuards(RoleGuard(Role.FullLovCategoryAccess, Role.FindAllLovCategory))
+  @Post('findAll')
+  async findAll(@Request() req: any, @Body() findAllDto: FindAllDto) {
+    try {
+      const res = await Promise.all(
+        findAllDto?.data?.map((findPayload) => {
+          const standardParams = addStandardParameters(req.user, findPayload);
+          return this.mainService.findAll(
+            standardParams,
+            findAllDto?.pagination,
+          );
+        }),
+      );
+      if (!isEmpty(res) && !isEmpty(res[0])) {
+        return RestResponse.success(res, API_SUCCESS_MESSAGE);
+      } else {
+        return RestResponse.notFound(res);
+      }
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  @UseGuards(JwtAuthGuard)
+  // @UseGuards(RoleGuard(Role.FullLovCategoryAccess, Role.UpdateLovCategory))
+  @Post('update')
+  async update(@Request() req: any, @Body() updateDto: UpdateDto) {
+    try {
+      const res = await Promise.all(
+        updateDto?.data?.map((updatePayload) => {
+          const standardParams = addStandardParameters(req.user, updatePayload);
+          return this.mainService.update({
+            ...standardParams,
+            dmlStatus: LID_UPDATE_ID,
+            dmlTimestamp: dayjs().format(),
+          });
+        }),
+      );
+      if (!isEmpty(res)) {
+        return RestResponse.success(res, API_SUCCESS_MESSAGE);
+      } else {
+        return RestResponse.notFound(res);
+      }
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  @UseGuards(JwtAuthGuard)
+  // @UseGuards(RoleGuard(Role.FullLovCategoryAccess, Role.DeleteLovCategory))
+  @Post('delete')
+  async remove(@Request() req: any, @Body() deleteDto: DeleteDto) {
+    try {
+      const res = await Promise.all(
+        deleteDto.data.map((findPayload) => {
+          const standardParams = addStandardParameters(req.user, findPayload);
+          return this.mainService.delete({
+            ...standardParams,
+            dmlStatus: LID_DELETE_ID,
+            dmlTimestamp: dayjs().format(),
+          });
+        }),
+      );
+      if (!isEmpty(res)) {
+        return RestResponse.success(res, API_SUCCESS_MESSAGE);
+      } else {
+        return RestResponse.notFound(res);
+      }
+    } catch (e) {
+      throw e;
+    }
+  }
 
   // @Post()
   // create(@Body() createOrganizationDto: CreateOrganizationDto) {
--- src/organization/organization.service.ts
+++ src/organization/organization.service.ts
@@ -1,26 +1,183 @@
 import { Injectable } from '@nestjs/common';
-import { CreateOrganizationDto } from './dto/create-organization.dto';
-import { UpdateOrganizationDto } from './dto/update-organization.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Organization } from './entities/organization.entity';
+import { Not, Repository } from 'typeorm';
+import { OrganizationHistory } from './entities/organization-history.entity';
+import { CreateDataPayloadDto } from './dto/create.dto';
+import {
+  LID_DELETE_ID,
+  RECORD_EXISTS,
+  TRY_AGAIN_LATER,
+} from 'src/utils/constants';
+import { RestResponse } from 'src/utils/restResponse';
+import { FindAllDataPayloadDto } from './dto/find-all.dto';
+import { paginationDto } from 'src/utils/commonDtos.dto';
+import { isEmpty, isNumber } from 'lodash';
+import { FindOneDataPayloadDto } from './dto/find-one.dto';
+import { UpdateDataPayloadDto } from './dto/update.dto';
+import { DeleteDataPayloadDto } from './dto/delete.dto';
 
 @Injectable()
 export class OrganizationService {
-  create(createOrganizationDto: CreateOrganizationDto) {
-    return 'This action adds a new organization';
+  constructor(
+    @InjectRepository(Organization)
+    private readonly mainRepository: Repository<Organization>,
+    @InjectRepository(OrganizationHistory)
+    private readonly historyRepositry: Repository<OrganizationHistory>,
+  ) {}
+
+  async create(params: CreateDataPayloadDto) {
+    // check if record exists
+    const ifRecordExists = await this.mainRepository.findOne({
+      where: [
+        {
+          title: params.title,
+          dmlStatus: Not(LID_DELETE_ID),
+        },
+      ],
+    });
+    if (ifRecordExists) {
+      return RestResponse.notFound(params, RECORD_EXISTS);
+    }
+    // create a new record
+    const res = await this.mainRepository.save({ ...params });
+    // create history record
+    await this.historyRepositry.save({ ...res });
+    if (res) {
+      return [res];
+    } else {
+      // If creation fails, throw a BadRequestException.
+      return RestResponse.error(params, TRY_AGAIN_LATER);
+    }
   }
 
-  findAll() {
-    return `This action returns all organization`;
+  async findAll(params: FindAllDataPayloadDto, pagination: paginationDto) {
+    let sql = '';
+    // Construct SQL query based on provided filter parameters.
+    if (isNumber(params?.organizationId)) {
+      sql += `r.organizationId=${params?.organizationId} AND `;
+    }
+    if (!isEmpty(params?.title)) {
+      sql += `r.title ilike '%${params?.title}%' AND `;
+    }
+    if (!isEmpty(params?.description)) {
+      sql += `r.description ilike '%${params?.description}%' AND `;
+    }
+    if (isNumber(params?.ownerId)) {
+      sql += `r.ownerId=${params?.ownerId} AND `;
+    }
+
+    sql += `r.dmlStatus != ${LID_DELETE_ID} ORDER BY 1 DESC`;
+
+    // Count the total number of records based on the constructed SQL query.
+    const count = await this.mainRepository
+      .createQueryBuilder('r')
+      .where(sql)
+      .getCount();
+    // Apply pagination if provided and return the filtered and paginated records.
+    if (
+      !isEmpty(pagination) &&
+      pagination?.pageNo >= 0 &&
+      pagination?.itemsPerPage > 0
+    ) {
+      sql += ` OFFSET ${
+        pagination?.pageNo * pagination?.itemsPerPage
+      } ROWS FETCH NEXT ${pagination?.itemsPerPage} ROWS ONLY`;
+    }
+
+    const query = count
+      ? await this.mainRepository.createQueryBuilder('r').where(sql).getMany()
+      : [];
+    return count ? [query, count] : [];
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} organization`;
+  async findOne(params: FindOneDataPayloadDto) {
+    const res = await this.mainRepository.findOne({
+      where: {
+        organizationId: params?.organizationId,
+        dmlStatus: Not(LID_DELETE_ID),
+      },
+    });
+
+    return res;
   }
 
-  update(id: number, updateOrganizationDto: UpdateOrganizationDto) {
-    return `This action updates a #${id} organization`;
+  async update(params: UpdateDataPayloadDto) {
+    // check if record exists
+    const ifRecordExists = await this.mainRepository.findOne({
+      where: [
+        {
+          title: params.title,
+          dmlStatus: Not(LID_DELETE_ID),
+          organizationId: Not(params.organizationId),
+        },
+      ],
+    });
+
+    if (ifRecordExists) {
+      return RestResponse.error(params, RECORD_EXISTS);
+    }
+
+    const obj = await this.findOne({
+      organizationId: params.organizationId,
+    });
+
+    // If the record to update does not exist, throw a NotFoundException.
+    if (!obj) {
+      return RestResponse.notFound(params);
+    }
+
+    const res = await this.mainRepository.save({
+      ...params,
+    });
+    await this.historyRepositry.save({ ...res });
+    if (!res) {
+      return [res];
+    } else {
+      // If creation fails, throw a BadRequestException.
+      return RestResponse.error(params, TRY_AGAIN_LATER);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} organization`;
+  async delete(params: DeleteDataPayloadDto) {
+    const obj = await this.findOne({
+      organizationId: params.organizationId,
+    });
+
+    // If the record to update does not exist, throw a NotFoundException.
+    if (!obj) {
+      return RestResponse.notFound(params);
+    }
+    obj.dmlStatus = params['dmlStatus'];
+    obj.dmlTimestamp = params['dmlTimestamp'];
+    const res = await this.mainRepository.save({
+      ...obj,
+    });
+    await this.historyRepositry.save({ ...res });
+    if (res) {
+      return [res];
+    } else {
+      // If creation fails, throw a BadRequestException.
+      return RestResponse.error(params, TRY_AGAIN_LATER);
+    }
   }
+  // create(createOrganizationDto: CreateOrganizationDto) {
+  //   return 'This action adds a new organization';
+  // }
+
+  // findAll() {
+  //   return `This action returns all organization`;
+  // }
+
+  // findOne(id: number) {
+  //   return `This action returns a #${id} organization`;
+  // }
+
+  // update(id: number, updateOrganizationDto: UpdateOrganizationDto) {
+  //   return `This action updates a #${id} organization`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} organization`;
+  // }
 }
