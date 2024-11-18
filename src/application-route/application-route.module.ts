9abe9b99ea10738ee9e49a453c8e666a97929ce3
Muhammad Owais <rahneverd@gmail.com>
2024-11-18 16:00:13 +0500

    updated applicationRoute module

--- src/application-route/application-route.module.ts
+++ src/application-route/application-route.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ApplicationRouteService } from './application-route.service';
 import { ApplicationRouteController } from './application-route.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApplicationRoute } from './entities/application-route.entity';
+import { ApplicationRouteHistory } from './entities/application-route-history.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([ApplicationRoute, ApplicationRouteHistory]),
+  ],
   controllers: [ApplicationRouteController],
   providers: [ApplicationRouteService],
 })
