6a4601028c3e14cdc07b5aaf0f5d4c4e0be35571
Muhammad Owais <rahneverd@gmail.com>
2024-11-20 13:40:25 +0500

    removed commented code from application route service

--- src/application-route/application-route.service.ts
+++ src/application-route/application-route.service.ts
@@ -173,20 +173,4 @@ export class ApplicationRouteService {
       return RestResponse.error(params, TRY_AGAIN_LATER);
     }
   }
-
-  // create(createApplicationRouteDto: CreateApplicationRouteDto) {
-  //   return 'This action adds a new applicationRoute';
-  // }
-  // findAll() {
-  //   return `This action returns all applicationRoute`;
-  // }
-  // findOne(id: number) {
-  //   return `This action returns a #${id} applicationRoute`;
-  // }
-  // update(id: number, updateApplicationRouteDto: UpdateApplicationRouteDto) {
-  //   return `This action updates a #${id} applicationRoute`;
-  // }
-  // remove(id: number) {
-  //   return `This action removes a #${id} applicationRoute`;
-  // }
 }
