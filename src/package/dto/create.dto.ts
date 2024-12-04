b59716814df6fcf4ef6b369c8251db822f71c7dd
Muhammad Owais <rahneverd@gmail.com>
2024-12-04 16:01:57 +0500

    inprogress - #10

---
+++ src/package/dto/create.dto.ts
@@ -0,0 +1,31 @@
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+
+export class CreateDataPayloadDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  lovStatusId: number;
+}
+
+export class CreateDto {
+  @IsNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => CreateDataPayloadDto)
+  @IsArray()
+  data: CreateDataPayloadDto[];
+}
---
+++ src/package/dto/update.dto.ts
@@ -0,0 +1,35 @@
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+
+export class UpdateDataPayloadDto {
+  @IsNotEmpty()
+  @IsNumber()
+  applicationId: number;
+
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  lovStatusId?: number;
+}
+
+export class UpdateDto {
+  @IsNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => UpdateDataPayloadDto)
+  @IsArray()
+  data: UpdateDataPayloadDto[];
+}
